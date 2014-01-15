263dd1da5b3c357373300868c7219440c13a3cad
= <greggjs@miamioh.edu>
2014-01-15 22:07:31 +0000

    Fixed everything but a serializer

--- rememerme/friends/rest/forms.py
+++ rememerme/friends/rest/forms.py
@@ -12,7 +12,7 @@ import bcrypt
 from rememerme.friends.models import Friends
 from config import util
 from rememerme.friends.rest.exceptions import FriendConflictException, FriendNotFoundException
-from rememerme.friends.serializers import FriendSerializer
+from rememerme.friends.serializers import FriendsSerializer
 from uuid import UUID
 from pycassa.cassandra.ttypes import NotFoundException as CassaNotFoundException
 
@@ -30,7 +30,7 @@ class FriendsPostForm(forms.Form):
         self.cleaned_data['active'] = True
         self.cleaned_data['facebook'] = self.cleaned_data['facebook'] if 'facebook' in self.cleaned_data else False
         self.cleaned_data['salt'] = bcrypt.gensalt()
-        self.cleaned_data['password'] = User.hash_password(self.cleaned_data['password'], self.cleaned_data['salt'])
+        self.cleaned_data['password'] = Friends.hash_password(self.cleaned_data['password'], self.cleaned_data['salt'])
         return self.cleaned_data
     
     '''
@@ -43,14 +43,14 @@ class FriendsPostForm(forms.Form):
         @return: A list of users matching the query with the given offset/limit
     '''
     def submit(self):
-        user = User.fromMap(self.cleaned_data)
+        user = Friends.fromMap(self.cleaned_data)
         # check if username and email have not been used yet
         # if they have not then save the user
-        if User.getByEmail(user.email) or User.getByUsername(user.username):
-            raise UserConflictException()
+        if Friends.getByEmail(user.email) or Friends.getByUsername(user.username):
+            raise FriendConflictException()
         
         user.save()
-        return UserSerializer(user).data
+        return FriendsSerializer(user).data
         
 class FriendsGetListForm(forms.Form):
     page = forms.CharField(required=False)
@@ -84,18 +84,18 @@ class FriendsGetListForm(forms.Form):
     '''
     def submit(self):
         if 'username' in self.cleaned_data:
-            ans = User.getByUsername(self.cleaned_data['username'])
-            uResponse = UserSerializer([] if not ans else [ans], many=True).data
+            ans = Friends.getByUsername(self.cleaned_data['username'])
+            uResponse = FriendsSerializer([] if not ans else [ans], many=True).data
             response = { 'data' : uResponse }
             return response
         elif 'email' in self.cleaned_data:
-            ans = User.getByEmail(self.cleaned_data['email'])
-            uResponse = UserSerializer([] if not ans else [ans], many=True).data
+            ans = Friends.getByEmail(self.cleaned_data['email'])
+            uResponse = FriendsSerializer([] if not ans else [ans], many=True).data
             response = { 'data' : uResponse }
             return response
         else:
-            ans = User.all(page=self.cleaned_data['page'], limit=self.cleaned_data['limit'])
-            uResponse = UserSerializer(ans, many=True).data
+            ans = Friends.all(page=self.cleaned_data['page'], limit=self.cleaned_data['limit'])
+            uResponse = FriendsSerializer(ans, many=True).data
             response = { 'data' : uResponse }
             if ans:
                 response['next'] = ans[-1].user_id
@@ -109,7 +109,7 @@ class FriendsGetSingleForm(forms.Form):
             self.cleaned_data['user_id'] = UUID(self.cleaned_data['user_id'])
             return self.cleaned_data
         except ValueError:
-            raise UserNotFoundException()
+            raise FriendNotFoundException()
     
     '''
         Submits a form to retrieve a user given the user_id.
@@ -118,12 +118,13 @@ class FriendsGetSingleForm(forms.Form):
     '''
     def submit(self):
         try:
-            ans = User.getByID(self.cleaned_data['user_id'])
+            ans = Friends.getByID(self.cleaned_data['user_id'])
             if not ans:
-                raise UserNotFoundException()
+                raise FriendsNotFoundException()
         except CassaNotFoundException:
-            raise UserNotFoundException()
-        return UserSerializer(ans).data
+            raise FriendNotFoundException()
+
+        return FriendsSerializer(ans).data
     
 class FriendsPutForm(forms.Form):
     username = forms.CharField(required=False)
@@ -132,11 +133,11 @@ class FriendsPutForm(forms.Form):
     user_id = forms.CharField(required=True)
     
     def clean(self):
-        cleaned_data = super(UserPutForm, self).clean()
+        cleaned_data = super(FriendsPutForm, self).clean()
         try:
             cleaned_data['user_id'] = UUID(cleaned_data['user_id'])
         except ValueError:
-            raise UserNotFoundException()
+            raise FriendNotFoundException()
         
         if not cleaned_data['email']: del cleaned_data['email']
         
@@ -152,30 +153,30 @@ class FriendsPutForm(forms.Form):
         
         # get the original user
         try:
-            user = User.get(user_id)
+            user = Friends.get(user_id)
         except CassaNotFoundException:
-            raise UserNotFoundException()
+            raise FriendNotFoundException()
         
         if not self.cleaned_data: # no real changes made
-            return UserSerializer(user).data
+            return FriendsSerializer(user).data
     
         # check to see username or email are being changed
         # if they are maintain the uniqueness
         if 'username' in self.cleaned_data:
-            if user.username != self.cleaned_data['username'] and User.get(username=self.cleaned_data['username']):
-                raise UserConflictException()
+            if user.username != self.cleaned_data['username'] and Friends.get(username=self.cleaned_data['username']):
+                raise FriendsConflictException()
         
         if 'email' in self.cleaned_data:
-            if user.email != self.cleaned_data['email'] and User.get(email=self.cleaned_data['email']):
-                raise UserConflictException()
+            if user.email != self.cleaned_data['email'] and Friends.get(email=self.cleaned_data['email']):
+                raise FriendsConflictException()
             
         if 'password' in self.cleaned_data:
-            self.cleaned_data['password'] = User.hash_password(self.cleaned_data['password'], user.salt)
+            self.cleaned_data['password'] = Friends.hash_password(self.cleaned_data['password'], user.salt)
         
         user.update(self.cleaned_data)
         user.save()
         
-        return UserSerializer(user).data
+        return FriendsSerializer(user).data
     
 
     
